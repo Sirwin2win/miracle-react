18c4bfc29ace6c5a07e33f0140206f01670dd1f8
Sirwin <sirwin2win@gmail.com>
2025-12-24 10:59:40 +0100

    good

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path='/about'  element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      </Route> 
+    </Routes>
+    </BrowserRouter>
   )
 }
 
