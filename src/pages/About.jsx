3e8efa4354364413b797b0a3949769597b606692
Sirwin <sirwin2win@gmail.com>
2025-12-29 12:00:53 +0100

    react css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Style.css'
+import styles from './styles.module.css'
 
+// onclick
 const About = () => {
+  const handleClick = ()=>{
+    // return alert('button clicked')
+    console.log('you clicked me!')
+  }
   return (
-    <div>About</div>
+    <div>
+      <h1 id='demo' onClick={handleClick}>Header 1</h1>
+      <h1 className={styles.name}>Header 1 second</h1>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
 const Home = () => {
+  const color = 'green'
+  const col = {
+    favCol:color
+  }
+
+  const demo = {
+    color:'orange',
+    fontFamily:'chiller',
+    fontSize:'100px'
+  }
   return (
     <div >
         <h1 className='text-primary'>Home Page</h1>
+        {/* 
+        CSS in react
+        1. inline css This goes as objects
+        2. external css
+        3. internal css
+        4. css module
+        */}
+        <h2 style={{color:'red'}}>Header 2</h2>
+        <h3 style={{color:col.favCol}}>Header 3 {col.favCol}</h3>
+        <p style={{color:"fuchsia",fontSize:'50px',fontWeight:'900'}}>Paragraph</p>
+        {/* Internal CSs */}
+        <h4 style={demo}>Heade Four</h4>
     </div>
   )
 }
